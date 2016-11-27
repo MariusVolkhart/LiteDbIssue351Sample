bb8a835128033328ca804566ae8e0f12159e01b6
Marius Volkhart <Marius@volkhart.com>
2016-11-27 20:16:22 +0100

    Sample program

---
+++ ConsoleApplication/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.IO;
+using LiteDB;
+
+namespace ConsoleApplication
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            using (var database = new LiteDatabase(new MemoryStream()))
+            {
+                var doc1 = new BsonDocument {["name"] = "john doe"};
+                var people = database.GetCollection("people");
+                people.Insert(doc1);
+                people.FindOne(Query.EQ("name", "john doe"));
+            }
+        }
+    }
+}
